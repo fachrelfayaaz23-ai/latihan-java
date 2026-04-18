10cdbf9053d4bf46854a68b4832088d1660bc415
Chapler <fachrelfayaaz23@gmail.com>
2026-04-19 02:02:58 +0700

    testing the train package

--- fachrel/routeTrainMap.java
+++ fachrel/routeTrainMap.java
@@ -1,5 +1,10 @@
 package fachrel;
 
+import shared.train;
+
 public class routeTrainMap {
-    
+    public static void main(String[] args) {
+        train t1 = new train("kereta", "T1");
+        System.out.println(t1);
+    }
 }
