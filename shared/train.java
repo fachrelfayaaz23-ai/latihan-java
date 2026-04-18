201122a0d29524da46e7dbe04fc913615889537e
Chapler <fachrelfayaaz23@gmail.com>
2026-04-19 01:53:39 +0700

    making public class A

---
+++ shared/train.java
@@ -0,0 +1,12 @@
+package shared;
+public class train{
+    private String name;
+    private String id;
+
+    public train(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+    
+    
+}
