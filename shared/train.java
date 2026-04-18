4199374c76a0b48784b9f2159c79c549f1030b71
Chapler <fachrelfayaaz23@gmail.com>
2026-04-19 02:26:31 +0700

    testing third branch

--- shared/train.java
+++ shared/train.java
@@ -1,16 +1,13 @@
 package shared;
 
 public class train{
-    private String name;
-    private String id;
+    String name;
+    String id;
 
     public train(String name, String id) {
         this.name = name;
         this.id = id;
     }
     
-    public static void main(String[] args) {
-        train t1 = new train("kereta", "T1");
-        System.out.println(t1);
-    }
+    
 }
